d492fb5a77f3c73a1e585ee784d870af06cecd67
Dongholy <dongholy@naver.com>
2023-04-10 19:00:04 +0900

    추가,삭제,체크,css 완료 // 수정 x

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,14 @@ import './App.css';
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [text, setText] = useState('');
-  const [done, setDone] = useState(false);
+
 
   const fetchD = () => {
      // 서버에 데이터를 요청하려면 서버 주소 , 어떤 method 필요
      fetch('http://localhost:4000/api/todo')
      .then((response) => response.json()) // 첫번째 응답값은 한번 Json 형태로 정제
      // .then((data) => console.log(data))
-     .then((data) => setTodoList(data)) // 진짜 나오는 데이터가 투두 리스트 레이터
+     .then((data) => setTodoList(data)) // 진짜 나오는 데이터가 투두 리스트 데이터
 
     //  axios ver.
     // axios.get('http://localhost:4000/api/todo').then((response) => setTodoList(response.data))
@@ -26,7 +26,7 @@ function App() {
     // fetch('http://localhost:4000/api/todo')
     // .then((response) => response.json()) // 첫번째 응답값은 한번 Json 형태로 정제
     // // .then((data) => console.log(data))
-    // .then((data) => setTodoList(data)) // 진짜 나오는 데이터가 투두 리스트 레이터
+    // .then((data) => setTodoList(data)) // 진짜 나오는 데이터가 투두 리스트 데이터
   }, []);
 
 
@@ -64,34 +64,36 @@ function App() {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        done: !done // toggle done
+        done: !done, // toggle done
+        text: todoList.find(todo => todo.id === id).text
       }),
     }).then(() => fetchD());
   }
 
   return (
     <div className="App">
-      <h1>todoList</h1>
+      <h1>오늘...뭐하지?</h1>
       <form onSubmit={onSubmitHandler}>
          <input
-          name='text'
+          className='text'
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <input type='submit' value="추가" />
+        <input type='submit' value="추가" className='plusbtn'/>
       </form>
       {todoList.map((todo) => (
-        <div key={todo.id}>
+        <div key={todo.id} className="tododata">
           <div>{todo.id}</div>
-          <div>{todo.text}</div>
+          <div className={todo.done ? 'text-line-through' : ''}>{todo.text}</div>
           <div>
             {/* checkbox를 통해 done 여부를 선택 */}
+            {todo.done ? '완료' : '도전중'}
             <input
               type='checkbox'
               checked={todo.done}
               onChange={() => onCheckHandler(todo.id, todo.done)}
             />
-            {todo.done ? '완료' : '도전중'}
+            
           </div>
           <button onClick={() => onDeleteHandler(todo.id)}>삭제</button>
           
